9c5ba0933947423a3a7d756cdc082d4a950a8bc5
Танаев Роман <romolus@list.ru>
2014-12-26 17:17:44 +0300

    20141226window

---
+++ SimpleMapEditor/SimpleWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine.Controllers;
+using Engine.Views;
+using Button = Engine.Views.Templates.Button;
+
+namespace SimpleMapEditor
+{
+	/// <summary>
+	/// Тестовое окно с кнопкой
+	/// </summary>
+	class SimpleWindow:ViewWindow
+	{
+		public SimpleWindow(Controller controller, ViewComponent parentSystem = null) : base(controller, parentSystem)
+		{
+			 //TODO --->>>> ControlDraggable получает курсор, но передаёт его только viewDraggable. нужно это как то исправить
+			AddComponent(Button.CreateButton(controller, 10, 10, 20, 20, "UU", "Кнопка", "S", Keys.U, "UU"));
+			this.Name = "SW";
+		}
+
+
+	}
+}
